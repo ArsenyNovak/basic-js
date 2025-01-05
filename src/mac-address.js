da9983356370602d44a9da701380ccfd2f397f1e
arseni_Ub <novakarsenya@gmail.com>
2025-01-05 18:26:00 +0300

    feat: solve mac-adress, sort-by-height

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,8 +14,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  array16 = ['1','2','3','4','5','6','7','8','9','0','A','B','C','D','E','F']
+  let arr = n.split('-')
+  if (arr.length !== 6) {
+    return false
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length !== 2) {
+      return false
+    }
+    if (!array16.includes(arr[i][0]) || !array16.includes(arr[i][1])){
+      return false
+    }
+  }
+  return true
   // remove line with error and write your code here
 }
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,8 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  let newArr = arr.filter((x) => x !== -1).sort((a,b) => a - b).reverse()
+  let finalArr = Array();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      finalArr.push(-1)
+    }
+    else {
+      finalArr.push(newArr.pop())
+    }
+  }
+  return finalArr
   // remove line with error and write your code here
 }
 
